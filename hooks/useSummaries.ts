eac3f9fa2f8c6d56c4030d3899fbe562492a341f
Solomon Olatunji <aotoluwalope@gmail.com>
2026-02-16 01:22:04 +0100

    refactor: update `useSummaries` query function to use async/await.

--- hooks/useSummaries.ts
+++ hooks/useSummaries.ts
@@ -6,10 +6,12 @@ import { WakaTimeSummaries } from '@/interfaces/summary';
 export function useSummaries(start: Date, end: Date) {
   return useQuery<WakaTimeSummaries>({
     queryKey: ['summaries', start, end],
-    queryFn: () =>
-      wakaService.getSummaries(
+    queryFn: async () => {
+      const data = await wakaService.getSummaries(
         start.toISOString().split('T')[0],
         end.toISOString().split('T')[0],
-      ),
+      );
+      return data;
+    },
   });
 }
